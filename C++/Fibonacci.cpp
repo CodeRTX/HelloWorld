406b8524f00d8871d79b92588112aa58dc121a50
Ankan Debnath <95306814+CodeRTX@users.noreply.github.com>
2021-12-11 23:13:18 +0530

    Create Fibonacci.cpp

---
+++ C++/Fibonacci.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+void main()
+{
+ clrscr();
+ long int a[100],n,i;
+ printf ("Enter the range: \n");
+ scanf ("%ld",&n);
+ a[0]=0;
+ a[1]=1;
+ for(i=2;i<n;i++)
+  a[i]=a[i-1]+a[i-2];                                 // This is an eg. of DynamicProgramming  (Tabulation Method)
+ for(i=0;i<n;i++)                                    // A Bottom-Up approach
+  printf ("%ld\t",a[i]);
+ getch();
+}
