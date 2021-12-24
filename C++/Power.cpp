ea5618bdd297d6e0ab8cdb5fcb1f8253c2e71817
Ankan Debnath <95306814+CodeRTX@users.noreply.github.com>
2021-12-24 21:17:15 +0530

    Update Power.cpp

--- C++/Power.cpp
+++ C++/Power.cpp
@@ -1,18 +1,19 @@
 /*This is an eg. of Recursion.*/
 
+
 #include<iostream.h>
 #include<conio.h>
 
-long double powr(long double x,int y)
+long double myPow(long double x,int y)
 {
  if(y<0)
-  return (1/powr(x,-y));
+  return (1/myPow(x,-y));
  if(y%2!=0)
-  return (x*powr(x,y/2)*powr(x,y/2));
+  return (x*myPow(x,y-1));
  if(y==0)
   return 1;
  else
-  return (powr(x,y/2)*powr(x,y/2));
+  return (myPow(x,y/2)*myPow(x,y/2));
 }
 
 void main()
@@ -20,8 +21,8 @@ void main()
  clrscr();
  long double a;
  int n;
- cout<<"Enter the no.s : ";
- cin>>a>>n;
- cout<<"The value is "<<powr(a,n)<<endl;
+ cout <<"Enter the no.s : ";
+ cin >>a>>n;
+ cout <<"The value is "<<myPow(a,n)<<endl;
  getch();
 }
